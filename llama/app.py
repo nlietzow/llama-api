f61aef471f3c59c09504088ccffd3974061b49b2
Niclas Lietzow <niclas.lietzow@outlook.com>
2023-08-24 11:40:53 +0200

    add proper error handling

--- llama/app.py
+++ llama/app.py
@@ -1,5 +1,5 @@
-from fastapi import FastAPI, Depends, Body, Query
-
+from fastapi import FastAPI, Depends, Body, Query, HTTPException
+import traceback
 from llama.application.authenticator import verify_token
 from llama.application.model_services import predict
 from llama.generation import Dialog, ChatPrediction
@@ -23,18 +23,23 @@ example_dialog = [
 @app.post("/predict", dependencies=[Depends(verify_token)])
 async def predict_nli(
     input_dialogs: list[Dialog] = Body(example=example_dialog),
-    temperature: float = Query(0.6, example=0.6),
-    top_p: float = Query(0.9, example=0.9),
+    temperature: float = Query(0, example=0.6),
+    top_p: float = Query(1, example=0.9),
     max_gen_len: int | None = Query(None, example=100),
     logprobs: bool = Query(False, example=False),
 ) -> list[ChatPrediction]:
-    return predict(
-        dialogs=input_dialogs,
-        temperature=temperature,
-        top_p=top_p,
-        max_gen_len=max_gen_len,
-        logprobs=logprobs,
-    )
+    try:
+        return predict(
+            dialogs=input_dialogs,
+            temperature=temperature,
+            top_p=top_p,
+            max_gen_len=max_gen_len,
+            logprobs=logprobs,
+        )
+    except Exception as e:
+        traceback_str = "".join(traceback.format_tb(e.__traceback__))
+        error_msg = f"{traceback_str}\n\nERROR: {repr(e)}"
+        raise HTTPException(status_code=500, detail=error_msg)
 
 
 if __name__ == "__main__":
