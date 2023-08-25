70ed0dffbf476df83a8c729b2e10785adf5c51c6
Niclas Lietzow <niclas.lietzow@outlook.com>
2023-08-25 03:23:47 +0200

    put app in debug mode

--- llama/app.py
+++ llama/app.py
@@ -1,45 +1,28 @@
-from fastapi import FastAPI, Depends, Body, Query, HTTPException
-import traceback
+from fastapi import FastAPI, Depends, Body, Query
+
 from llama.application.authenticator import verify_token
+from llama.application import examples
 from llama.application.model_services import predict
 from llama.generation import Dialog, ChatPrediction
 
-app = FastAPI()
-
-example_dialog = [
-    [
-        {
-            "role": "system",
-            "content": "Always answer with emojis",
-        },
-        {
-            "role": "user",
-            "content": "How to go from Beijing to NY?",
-        },
-    ],
-]
+app = FastAPI(debug=True)
 
 
 @app.post("/predict", dependencies=[Depends(verify_token)])
 async def predict_nli(
-    input_dialogs: list[Dialog] = Body(example=example_dialog),
+    input_dialogs: list[Dialog] = Body(example=examples.example_dialog),
     temperature: float = Query(0, example=0.6),
     top_p: float = Query(1, example=0.9),
     max_gen_len: int | None = Query(None, example=100),
     logprobs: bool = Query(False, example=False),
 ) -> list[ChatPrediction]:
-    try:
-        return predict(
-            dialogs=input_dialogs,
-            temperature=temperature,
-            top_p=top_p,
-            max_gen_len=max_gen_len,
-            logprobs=logprobs,
-        )
-    except Exception as e:
-        traceback_str = "".join(traceback.format_tb(e.__traceback__))
-        error_msg = f"{traceback_str}\n\nERROR: {repr(e)}"
-        raise HTTPException(status_code=500, detail=error_msg)
+    return predict(
+        dialogs=input_dialogs,
+        temperature=temperature,
+        top_p=top_p,
+        max_gen_len=max_gen_len,
+        logprobs=logprobs,
+    )
 
 
 if __name__ == "__main__":
---
+++ llama/application/examples.py
@@ -0,0 +1,12 @@
+example_dialog = [
+    [
+        {
+            "role": "system",
+            "content": "Always answer with emojis",
+        },
+        {
+            "role": "user",
+            "content": "How to go from Beijing to NY?",
+        },
+    ],
+]
